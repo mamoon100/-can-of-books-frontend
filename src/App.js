6312e590857dec2a25749b795fb71d5a9eec41a7
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-08-15 16:27:21 +0300

    finished lab11 (added login and logout)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import Header from "./Header";
+import IsLoadingAndError from "./IsLoadingAndError";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { withAuth0 } from "@auth0/auth0-react";
+import Login from "./Login";
+import BestBooks from "./BestBooks";
+import Profile from "./Profile";
 
 class App extends React.Component {
-
   render() {
-    console.log('app', this.props);
-    return(
+    console.log("app", this.props);
+    return (
       <>
         <Router>
           <IsLoadingAndError>
             <Header />
             <Switch>
               <Route exact path="/">
+                {this.props.auth0.isAuthenticated ? <BestBooks /> : <Login />}
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+              </Route>
+              <Route exact path="/profile">
+                {this.props.auth0.isAuthenticated && (
+                  <Profile
+                    name={this.props.auth0.user.name}
+                    src={this.props.auth0.user.picture}
+                    email={this.props.auth0.user.email}
+                  />
+                )}
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
             </Switch>
@@ -31,4 +40,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withAuth0(App);
